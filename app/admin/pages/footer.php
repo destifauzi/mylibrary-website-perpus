b70e07befa9088118dddde4aadac305f811fa4c0
desti <destinurfauzirahmawati@gmail.com>
2024-03-07 23:25:19 +0700

    first commit

---
+++ app/admin/pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.0
+    </div>
+    <strong>Kelompok 4</strong> Desti-Devy-Fahrizal-Indira.
+</footer>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+// Arahkan user ketika pertama kali membuka applikasi ke halaman login
+header("location: masuk");
